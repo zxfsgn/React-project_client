893452db8db934b1c1b0559c8b22ab8da8ee9d9c
zxfsgn <makarov011099@gmail.com>
2021-08-18 16:32:47 +0300

    master

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -8,7 +8,7 @@ export const Registration = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await fetch('http://localhost:5000/api/auth/register', {
+    await fetch('https://mighty-cliffs-11442.herokuapp.com/api/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
