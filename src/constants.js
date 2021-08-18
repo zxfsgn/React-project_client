9a6e6616c5171c1a61abe90ca4c93b0bf3b84ae9
zxfsgn <makarov011099@gmail.com>
2021-08-18 13:47:35 +0300

    master

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const ROOT_URL = "http://localhost:5000/api"
+export const ROOT_URL = "https://mighty-cliffs-11442.herokuapp.com/api"
 export const NEWS_URI = `${ROOT_URL}/content/news`
 export const PROFILE_URI = `${ROOT_URL}/content/profile`
